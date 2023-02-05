08076f2f70e5369b1c29e2158036ac2eac6e2ab3
Abhishek Pandey <123861746+abhi759724@users.noreply.github.com>
2023-02-06 04:15:46 +0530

    comment

---
+++ While-Loops-IW/problem3.js
@@ -0,0 +1,11 @@
+i=1;
+sum=0;
+while(i<=60) {
+  if(i%3==0) {
+    sum+=i
+  }
+  
+  i++
+  
+}
+console.log(sum);
---
+++ While-Loops-IW/problem4.js
@@ -0,0 +1,7 @@
+i=1;
+sum=0;
+while(i<=100) {
+  sum+=i
+  i++
+}
+console.log(sum/100);
